f5aec4f7a6264c6813278d7f3d12a8a75065af3d
kalizn <netotads2019@gmail.com>
2021-11-22 09:06:36 -0300

    V.05

--- App.js
+++ App.js
@@ -8,18 +8,23 @@ export default function App() {
   function executarCalculos() {
     const alt = altura / 100;
     const imc = peso / (alt*alt);
+
+    
     
     if (imc < 18.6) {
       alert('Você está abaixo do peso - IMC = ' + imc.toFixed(2));
     } else if (imc >= 18.6 && imc < 24.9) {
       alert('Você está com o peso ideal - IMC = ' + imc.toFixed(2));
     } else if (imc >= 24.9 && imc < 34.9) {
-      alert('Você está abaixo do peso - IMC = ' + imc.toFixed(2));
+      alert('Você está levemente acima do peso - IMC = ' + imc.toFixed(2));
     } else if (imc >= 34.9) {
       alert('Você está acima do peso - IMC = ' + imc.toFixed(2));
     }
-
+    setPeso('');
+    setAltura('');
+    
   }
+    
 
   return (
     <View style={estilo.container}>
